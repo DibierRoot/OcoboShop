ae37324da21a51796990b9bb8142cb00ec77ca26
DibierRoot <criscami@gmail.com>
2025-10-01 13:37:25 -0500

    subir a la web

--- src/components/InicioNoLogeado/Header.jsx
+++ src/components/InicioNoLogeado/Header.jsx
@@ -240,7 +240,7 @@ const Header = ({login}) => {
         })
 
         try {
-            const response = await axios.post('https://ocoboshopback.lovestoblog.com/OcoboBack-end/CRUD/', {
+            const response = await axios.post('https://ocoboshopback.lovestoblog.com/OcoboBack-end/CRUD/index.php', {
                 action: "login",
                 correo,
                 contrasena
