80d7414bcd2b734603210313821111cfac9f639d
DibierRoot <criscami@gmail.com>
2025-10-01 13:39:37 -0500

    subir a la web

--- src/components/InicioNoLogeado/Main.jsx
+++ src/components/InicioNoLogeado/Main.jsx
@@ -184,7 +184,7 @@ const Main = () => {
 
   // Obtener los productos desde el backend
   const obtenerDatos = async () => {
-      axios.get('http://ocoboshopback.lovestoblog.com/OcoboBack-end/VerProductos/')  // Ruta del archivo PHP
+      axios.get('https://ocoboshopback.lovestoblog.com/OcoboBack-end/VerProductos/index.php')  // Ruta del archivo PHP
         .then(response => {
           setVerProductos(response.data);  // Guardamos los datos de los productos en el estado
         })
